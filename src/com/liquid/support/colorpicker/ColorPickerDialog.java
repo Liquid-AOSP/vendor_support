a2d0eea59e065d7a1f6651f55b655e095c52cd25
rk134 <rahul-k@bigdi.cc>
2022-05-30 19:06:10 +0100

    support: initial rebrand to liquid
    * thanks arrow :)
    
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- src/com/liquid/support/colorpicker/ColorPickerDialog.java
+++ src/com/liquid/support/colorpicker/ColorPickerDialog.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.arrow.support.colorpicker;
+package com.liquid.support.colorpicker;
 
 import android.app.AlertDialog;
 import android.app.NotificationManager;
@@ -31,7 +31,7 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 
-import com.arrow.support.R;
+import com.liquid.support.R;
 
 public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener {
 
